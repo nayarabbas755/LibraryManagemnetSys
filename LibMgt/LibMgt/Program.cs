35ffdd517bafd46bcfd7c6ace1750681fcdf78e2
nayarabbas755 <161396490+nayarabbas755@users.noreply.github.com>
2024-03-05 16:24:10 +0500

    program.cs update

--- LibMgt/LibMgt/Program.cs
+++ LibMgt/LibMgt/Program.cs
@@ -11,11 +11,11 @@ builder.Services.Configure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
